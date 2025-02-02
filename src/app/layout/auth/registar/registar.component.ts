e7674b463e14ca9e6b93bdf96cf13c05c44d02ae
adelNfahmy10 <fahmyan@taibaconnect.com>
2025-02-02 14:14:40 +0200

    install bootstrap and fontawesome and create layout auth and blanck

---
+++ src/app/layout/auth/registar/registar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registar',
+  standalone: true,
+  imports: [],
+  templateUrl: './registar.component.html',
+  styleUrl: './registar.component.scss'
+})
+export class RegistarComponent {
+
+}
