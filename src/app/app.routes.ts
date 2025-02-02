d626ae81c3ebdffa96a29223f25829988b57d7ba
adelNfahmy10 <fahmyan@taibaconnect.com>
2025-02-02 17:05:05 +0200

    create components blanck

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { NavbarComponent } from './layout/blanck/navbar/navbar.component';
+import { DashboardComponent } from './layout/blanck/dashboard/dashboard.component';
+import { FooterComponent } from './layout/blanck/footer/footer.component';
+import { NotfoundComponent } from './layout/blanck/notfound/notfound.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',redirectTo:'dashboard', pathMatch:'full'},
+    {path:'navbar', component:NavbarComponent, title:'Navbar'},
+    {path:'dashboard', component:DashboardComponent, title:'dashboard'},
+    {path:'footer', component:FooterComponent, title:'footer'},
+    {path:'**', component:NotfoundComponent, title:'notfound'},
+];
