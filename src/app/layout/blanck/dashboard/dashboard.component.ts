25b97b3a2c1e59188a527d6837d833f7dfffffe1
adelNfahmy10 <fahmyan@taibaconnect.com>
2025-02-10 16:56:25 +0200

    remove routerLink

--- src/app/layout/blanck/dashboard/dashboard.component.ts
+++ src/app/layout/blanck/dashboard/dashboard.component.ts
@@ -7,7 +7,7 @@ Chart.register(...registerables);
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [RouterLink],
+  imports: [],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
 })
