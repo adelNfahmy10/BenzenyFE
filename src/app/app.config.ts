79a6d9df1a540d3a45c18e59f26ade42a8eab0ce
adelNfahmy10 <fahmyan@taibaconnect.com>
2025-02-12 14:15:15 +0200

    add interceptor header and company Apis

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,14 +2,15 @@ import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter, withInMemoryScrolling, withViewTransitions } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideToastr } from 'ngx-toastr';
+import { headerInterceptor } from '../core/interceptor/header.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes, withViewTransitions(), withInMemoryScrolling({scrollPositionRestoration:'top'})),
     provideClientHydration(),
-    provideHttpClient(withFetch()),
+    provideHttpClient(withFetch(), withInterceptors([headerInterceptor])),
     provideAnimations(),
     provideToastr(),
   ]
--- src/app/layout/auth/registar/registar.component.ts
+++ src/app/layout/auth/registar/registar.component.ts
@@ -1,23 +1,24 @@
 import { Component, inject } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../core/service/auth.service';
 import { NgxDropzoneModule } from 'ngx-dropzone';
-import { NgFor, NgIf } from '@angular/common';
+import { NgFor } from '@angular/common';
+import { CompanyService } from '../../blanck/companies/core/service/company.service';
 
 @Component({
   selector: 'app-registar',
   standalone: true,
-  imports: [ReactiveFormsModule, NgxDropzoneModule, NgFor],
+  imports: [ReactiveFormsModule, NgxDropzoneModule, NgFor,FormsModule],
   templateUrl: './registar.component.html',
   styleUrl: './registar.component.scss'
 })
 export class RegistarComponent {
   private readonly _FormBuilder = inject(FormBuilder)
-  private readonly _AuthService = inject(AuthService)
+  private readonly _CompanyService = inject(CompanyService)
   private readonly _Router = inject(Router)
 
-
+  step:number = 1
   files: File[] = [];
 
   onSelect(event:any) {
@@ -32,28 +33,52 @@ export class RegistarComponent {
   }
 
   registerForm:FormGroup = this._FormBuilder.group({
-    fullName: [''],
-    username: [''],
-    email: [''],
-    mobile: [''],
-    password: [''],
-    companyName: [''],
-    description: [''],
-    file: [''],
+    Name: [''],
+    Description: [''],
+    CompanyEmail: [''],
+    CompanyPhone: [''],
+    Files: [''],
   })
-
+  companyId:string = ''
   submitRegisterForm():void{
     let data = this.registerForm.value
-    console.log(data);
-    this._AuthService.register(data).subscribe({
+    data.Files = this.files
+    let formData = new FormData()
+    formData.append('Name', data.Name)
+    formData.append('Description', data.Description)
+    formData.append('CompanyEmail', data.CompanyEmail)
+    formData.append('CompanyPhone', data.CompanyPhone)
+    formData.append('Files', data.Files)
+    this.step = 2
+    this.getCompanyById()
+
+    // this._CompanyService.CreateCompany(formData).subscribe({
+    //   next:(res)=>{
+    //     console.log(res);
+    //     this.companyId = res.data.id
+    //     this.step = 2
+    //     this.getCompanyById()
+    //   }
+    // })
+  }
+
+  companyData!:any
+  userName:string = ''
+  password:string = ''
+
+  getCompanyById():void{
+    this._CompanyService.GetCompanyById('727eddb5-d6de-4b53-a1ee-08dd4b57862b').subscribe({
       next:(res)=>{
-        console.log(res);
-        this._Router.navigate(['/login'])
-      },
-      error:(err)=>{
-        console.log(err);
+        this.companyData = res
+        console.log(this.companyData);
       }
     })
   }
-
+  continueData():void{
+    let data = {...this.companyData}
+    data.UserName = this.userName
+    data.UserName = this.password
+    console.log(data);
+    this._Router.navigate(['/home'])
+  }
 }
