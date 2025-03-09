9abe01f50f6c23d2917f35cccac4248d237d7729
adelNfahmy10 <fahmyan@taibaconnect.com>
2025-03-09 11:08:06 +0200

    register user for two steps is finish

--- src/app/layout/auth/registeruser/registeruser.component.ts
+++ src/app/layout/auth/registeruser/registeruser.component.ts
@@ -1,12 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { UsersService } from '../../blanck/companies/core/service/users.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-registeruser',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './registeruser.component.html',
   styleUrl: './registeruser.component.scss'
 })
-export class RegisteruserComponent {
+export class RegisteruserComponent implements OnInit{
+  private readonly _FormBuilder = inject(FormBuilder)
+  private readonly _UsersService = inject(UsersService)
+  private readonly _ActivatedRoute = inject(ActivatedRoute)
+  private readonly _ToastrService = inject(ToastrService)
+  private readonly _Router = inject(Router)
 
+  userId:string = ''
+  userData:any = {}
+  ngOnInit(): void {
+    this.getUserById()
+  }
+
+  getUserById():void{
+    this._ActivatedRoute.paramMap.subscribe({
+      next:(params)=>{
+        this.userId = params.get('userId')!
+        this._UsersService.getUserById(this.userId).subscribe({
+          next:(res)=>{
+            this.userData = res.data
+          }
+        })
+      }
+    })
+  }
+
+  userForm:FormGroup = this._FormBuilder.group({
+    username:[''],
+    password:[''],
+  })
+
+  submitUserForm():void{
+    let data = this.userForm.value
+    data.id = this.userId
+    let formData = new FormData()
+
+    this._UsersService.updateUser(data.id, data).subscribe({
+      next:(res)=>{
+        console.log(res);
+        this._ToastrService.success(res.msg)
+        // ROLES
+        // this._Router.navigate(['/driver'])
+        // this._Router.navigate(['/balance'])
+      }
+    })
+  }
 }
--- src/app/layout/blanck/companies/core/service/users.service.ts
+++ src/app/layout/blanck/companies/core/service/users.service.ts
@@ -1,13 +1,25 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { environment } from '../../../../../assets/environment/environment';
+import { environment } from '../../../../../../assets/environment/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
   private readonly _HttpClient = inject(HttpClient)
+  getAllUser():Observable<any>{
+    return this._HttpClient.get(`${environment.baseURL}api/UserManager/v1/GetAll/GetAll`)
+  }
+  getUserById(userId:any):Observable<any>{
+    return this._HttpClient.get(`${environment.baseURL}api/UserManager/v1/GetById/GetById/${userId}`)
+  }
+  getUsersCompany(companyId:any):Observable<any>{
+    return this._HttpClient.get(`${environment.baseURL}api/UserManager/v1/GetByCompanyId/GetByCompanyId/${companyId}`)
+  }
+  getUsersBranch(branchId:any):Observable<any>{
+    return this._HttpClient.get(`${environment.baseURL}api/UserManager/v1/GetByBranchId/GetByBranchId/${branchId}`)
+  }
   addUser(data:any):Observable<any>{
     return this._HttpClient.post(`${environment.baseURL}api/UserManager/v1/Register/register`,data)
   }
