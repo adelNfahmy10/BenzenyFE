271274d8e5727b6f1db203dc90d8a413bf6bb8b1
adelNfahmy10 <fahmyan@taibaconnect.com>
2025-03-23 13:57:26 +0200

    all img is lazyLoading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -24,29 +24,44 @@ import { UsersComponent } from './layout/blanck/companies/users/users.component'
 import { AdsComponent } from './layout/blanck/ads/ads.component';
 
 export const routes: Routes = [
-  {path:'', component:AuthComponent, canActivate:[logedGuard] , title:'login', children:[
-    {path:'', redirectTo:'login', pathMatch:'full'},
-    {path:'login', component:LoginComponent, title:'Login'},
-    {path:'register', component:RegistarComponent, title:'Register'},
-    {path:'contiuneRegistar/:companyId', component:ContinueregistarComponent, title:'Register'},
-    {path:'registeruser/:userId', component:RegisteruserComponent, title:'Register'},
-  ]},
-
-  {path:'', component:BlanckComponent, canActivate:[authGuard], title:'home', children:[
-    {path:'home', component:DashboardComponent, title:'Home'},
-    {path:'company', component:CompaniesComponent, title:'Company'},
-    {path:'users', component:UsersComponent, title:'Users'},
-    {path:'branch', component:BranchesComponent, title:'Branches'},
-    {path:'details/:id', component:BranchdetailsComponent, title:'Branch'},
-    {path:'balance', component:BalanceComponent, title:'Balance'},
-    {path:'cars', component:CarsComponent, title:'Cars'},
-    {path:'drivers', component:DriversComponent, title:'Drivers'},
-    {path:'petrolstation', component:PetrolstationComponent, title:'Petrol Stations'},
-    {path:'analytics', component:AnalyticsComponent, title:'analyics'},
-    {path:'ads', component:AdsComponent, title:'ADS'},
-    {path:'management', component:ManagementComponent, title:'Management'},
-    {path:'setting', component:SettingsComponent, title:'Setting'},
-    {path:'footer', component:FooterComponent, title:'footer'},
-  ]},
-  {path:'**', component:NotfoundComponent, title:'notfound'},
+  {
+    path: '',
+    loadComponent: () => import('./layout/auth/auth/auth.component').then(m => m.AuthComponent),
+    canActivate: [logedGuard],
+    title: 'login',
+    children: [
+      { path: '', redirectTo: 'login', pathMatch: 'full' },
+      { path: 'login', loadComponent: () => import('./layout/auth/login/login.component').then(m => m.LoginComponent), title: 'Login' },
+      { path: 'register', loadComponent: () => import('./layout/auth/registar/registar.component').then(m => m.RegistarComponent), title: 'Register' },
+      { path: 'contiuneRegistar/:companyId', loadComponent: () => import('./layout/auth/registar/continueregistar/continueregistar.component').then(m => m.ContinueregistarComponent), title: 'Register' },
+      { path: 'registeruser/:userId', loadComponent: () => import('./layout/auth/registeruser/registeruser.component').then(m => m.RegisteruserComponent), title: 'Register' },
+    ]
+  },
+  {
+    path: '',
+    loadComponent: () => import('./layout/blanck/blanck/blanck.component').then(m => m.BlanckComponent),
+    canActivate: [authGuard],
+    title: 'home',
+    children: [
+      { path: 'home', loadComponent: () => import('./layout/blanck/dashboard/dashboard.component').then(m => m.DashboardComponent), title: 'Home' },
+      { path: 'company', loadComponent: () => import('./layout/blanck/companies/companies.component').then(m => m.CompaniesComponent), title: 'Company' },
+      { path: 'users', loadComponent: () => import('./layout/blanck/companies/users/users.component').then(m => m.UsersComponent), title: 'Users' },
+      { path: 'branch', loadComponent: () => import('./layout/blanck/branches/branches.component').then(m => m.BranchesComponent), title: 'Branches' },
+      { path: 'details/:id', loadComponent: () => import('./layout/blanck/branches/branchdetails/branchdetails.component').then(m => m.BranchdetailsComponent), title: 'Branch' },
+      { path: 'balance', loadComponent: () => import('./layout/blanck/balance/balance.component').then(m => m.BalanceComponent), title: 'Balance' },
+      { path: 'cars', loadComponent: () => import('./layout/blanck/cars/cars.component').then(m => m.CarsComponent), title: 'Cars' },
+      { path: 'drivers', loadComponent: () => import('./layout/blanck/drivers/drivers.component').then(m => m.DriversComponent), title: 'Drivers' },
+      { path: 'petrolstation', loadComponent: () => import('./layout/blanck/petrolstation/petrolstation.component').then(m => m.PetrolstationComponent), title: 'Petrol Stations' },
+      { path: 'analytics', loadComponent: () => import('./layout/blanck/analytics/analytics.component').then(m => m.AnalyticsComponent), title: 'Analytics' },
+      { path: 'ads', loadComponent: () => import('./layout/blanck/ads/ads.component').then(m => m.AdsComponent), title: 'ADS' },
+      { path: 'management', loadComponent: () => import('./layout/blanck/management/management.component').then(m => m.ManagementComponent), title: 'Management' },
+      { path: 'setting', loadComponent: () => import('./layout/blanck/settings/settings.component').then(m => m.SettingsComponent), title: 'Setting' },
+      { path: 'footer', loadComponent: () => import('./layout/blanck/footer/footer.component').then(m => m.FooterComponent), title: 'Footer' },
+    ]
+  },
+  {
+    path: '**',
+    loadComponent: () => import('./layout/blanck/notfound/notfound.component').then(m => m.NotfoundComponent),
+    title: 'Not Found'
+  }
 ];
