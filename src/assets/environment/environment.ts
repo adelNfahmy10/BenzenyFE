d0ed48fb2d06f64943ac6edf916c453bd7f9e4fe
adelNfahmy10 <fahmyan@taibaconnect.com>
2025-03-26 16:13:57 +0200

    done

--- src/assets/environment/environment.ts
+++ src/assets/environment/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  baseURL : `https://benzeny.taibaconnect.com:200/`,
+  baseURL : `http://benzeny.taibaconnect.com:200/`,
   // baseURL : `http://192.168.40.33:200/`,
   // baseURL : `http://benzeny.taibaconnect.com/`,
   // baseURL : `http://192.168.40.33/`,
