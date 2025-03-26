82b648917d4b6d9b9fad01d7e9d92836097d51db
adelNfahmy10 <fahmyan@taibaconnect.com>
2025-03-26 15:39:31 +0200

    change baseURL

--- src/assets/environment/environment.ts
+++ src/assets/environment/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
-  baseURL : `http://benzeny.taibaconnect.com/`,
+  baseURL : `http://192.168.40.33:200/`,
+  // baseURL : `http://benzeny.taibaconnect.com/`,
   // baseURL : `http://192.168.40.33/`,
   // baseURL : `http://localhost:4200/`
 }
